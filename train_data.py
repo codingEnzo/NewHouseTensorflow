759760d7d02de7cacb9ccb3b42fc77705e1df2c3
Gssssssssy <coder.gsy@gmail.com>
2018-02-07 15:36:15 +0800

    TF - 徐州

--- train_data.py
+++ train_data.py
@@ -8,37 +8,45 @@ from tensorflow.python.estimator.inputs.pandas_io import pandas_input_fn
 from utils.multiapplier import mg, apply_by_multiprocessing
 from utils.normalizer import norm_by_mean
 
-raw_data = pd.read_csv('csv/clean_data_sample.csv', sep='\t', index_col='HouseID', encoding='utf-8')
-fmt_data = raw_data.drop(columns=['RealEstateProjectID', 'ParkingSpaceAmount.1', 'Unnamed: 0']).fillna(0)
 
-# Info
-ifo_data = fmt_data[['ProjectName', 'ClassifyResult', 'HouseUseType']]
+def load_data():
+    raw_data = pd.read_csv('csv/clean_data_sample.csv', sep='\t', index_col='HouseID', encoding='utf-8')
+    fmt_data = raw_data.drop(columns=['RealEstateProjectID', 'ParkingSpaceAmount.1', 'Unnamed: 0']).fillna(0)
 
-# 热编码处理
-tsf_data = fmt_data.drop(columns=['ProjectName', 'ClassifyResult', 'HouseUseType'])
-tsf_data = pd.get_dummies(tsf_data)
+    # Info
+    ifo_data = fmt_data[['ProjectName', 'ClassifyResult', 'HouseUseType']]
 
-# 归一化处理
-cpu_count = mg.cpu_count() - 1 if mg.cpu_count() > 1 else mg.cpu_count
-tsf_data = apply_by_multiprocessing(tsf_data, norm_by_mean, axis=1, workers=cpu_count)
+    # 热编码处理
+    tsf_data = fmt_data.drop(columns=['ProjectName', 'ClassifyResult', 'HouseUseType'])
+    tsf_data = pd.get_dummies(tsf_data)
 
-# 归一化数据留存
-dupe = pd.concat([ifo_data, tsf_data], axis=1)
-dupe.to_csv('csv/clean_data_sample_normalize_{}'.format(datetime.now()))
+    # 归一化处理
+    cpu_count = mg.cpu_count() - 1 if mg.cpu_count() > 1 else mg.cpu_count
+    tsf_data = apply_by_multiprocessing(tsf_data, norm_by_mean, axis=1, workers=cpu_count)
 
-# 训练样本
-train_x = tsf_data
-train_y = ifo_data['ClassifyResult']
+    # 归一化数据留存
+    dupe = pd.concat([ifo_data, tsf_data], axis=1)
+    dupe.to_csv('csv/clean_data_sample_normalize_{}'.format(datetime.now()))
+
+    # 训练样本
+    train_x = tsf_data
+    train_y = ifo_data['ClassifyResult']
+    return train_x, train_y
 
 
 def main():
+    train_x, train_y = load_data()
     xuzhou_feature_columns = []
+
+    for i, colname in enumerate(train_x.columns):
+        xuzhou_feature_columns.append(colname)
 
     classifier = tf.estimator.DNNClassifier(
         feature_columns=xuzhou_feature_columns,
         hidden_units=[40, 20],
+        model_dir='/tmp',
         n_classes=3)
 
     classifier.train(
-        input_fn=lambda: pandas_input_fn(train_x, train_y, ),
+        input_fn=lambda: pandas_input_fn(train_x, train_y),
         steps=1)
